e010de5712afd55a43f145be69a8e629de966b25
julio93wolf <taskforce141>
2017-05-19 13:52:03 -0500

    Sistema Experto:
    * Renombre del modulo de actualizacion

---
+++ sistema_experto/src/sistema_experto/modulo_actualizacion.java
@@ -0,0 +1,15 @@
+package sistema_experto;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ValleRo
+ */
+public class modulo_actualizacion {
+    
+}
